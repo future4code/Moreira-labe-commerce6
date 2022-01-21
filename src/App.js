9ff2dea07f24a93d398fb282075e68aadd7c1cb2
antoniosantos2 <antonio.santos2@ufpe.br>
2022-01-21 19:46:21 -0300

    tentando alterar

--- src/App.js
+++ src/App.js
@@ -9,72 +9,72 @@ class filtroProduto extends React.Component {
   state = {
     cestaProdutos: [{
       id: 1,
-	    name: "Foguete da Missão Apollo 11",
-	    value: 10000.0,
-	    imageUrl: "https://picsum.photos/200/200"
+      name: "Foguete da Missão Apollo 11",
+      value: 10000.0,
+      imageUrl: "https://picsum.photos/200/200"
     },
     {
-     id: 2,
-     name: "Foguete da Missão Apollo 12",
-     value: 20000.0,
-     imageUrl: "https://picsum.photos/200/200"
+      id: 2,
+      name: "Foguete da Missão Apollo 12",
+      value: 20000.0,
+      imageUrl: "https://picsum.photos/200/200"
     }
     ],
     precoMin: '',
-    precoMax:'',
-    buscaNome:''
+    precoMax: '',
+    buscaNome: ''
   };
 
   onChangePrecoMax = (event) => {
-    this.setState({precoMax: event.target.value})
+    this.setState({ precoMax: event.target.value })
 
   };
 
-  onChangePrecoMin = (event)=> {
-    this.setState({precoMin: event.target.value})
+  onChangePrecoMin = (event) => {
+    this.setState({ precoMin: event.target.value })
 
   };
 
-  onChangeBuscaNome = (event)=> {
-    this.setState({buscaNome: event.target.value})
+  onChangeBuscaNome = (event) => {
+    this.setState({ buscaNome: event.target.value })
 
   };
 
-  render () {
+  render() {
     return (
       <filtroProduto>
 
         <nav>
 
-        <h3>Filtros</h3>
+          <h3>Filtros</h3>
 
-        <div>
-          <p>Valor mínimo:</p>
+          <div>
+            <p>Valor mínimo:</p>
 
-          <input 
-          value={this.state.precoMin}
-          onChange={this.onChangePrecoMin}
-          />
+            <input
+              value={this.state.precoMin}
+              onChange={this.onChangePrecoMin}
+            />
 
-        </div>
+          </div>
 
-        <div>
-          <p>Valor máximo:</p>
+          <div>
+            <p>Valor máximo:</p>
 
-          <input 
-          value={this.state.precoMax}
-          onChange={this.onChangePrecoMax}
-          />
-        </div>
+            <input
+              value={this.state.precoMax}
+              onChange={this.onChangePrecoMax}
+            />
+          </div>
 
-        <div>
-          <p>Busca por nome:</p>
+          <div>
+            <p>Busca por nome:</p>
 
-          <input
-          value={this.state.buscaNome}
-          onChange={this.onChangeBuscaNome}
-          />
-        </div>
+            <input
+              value={this.state.buscaNome}
+              onChange={this.onChangeBuscaNome}
+            />
+          </div>
         </nav>
 
       </filtroProduto>
